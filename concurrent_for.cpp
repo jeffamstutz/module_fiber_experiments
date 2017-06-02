4abd18392876b37b489f9abb3f9b9f26ff51ad80
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2017-06-02 02:32:50 +0000

    exploring fiber management and tasking

--- concurrent_for.cpp
+++ concurrent_for.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <iostream>
 
 #include <boost/fiber/all.hpp>
@@ -5,38 +6,88 @@
 const int N_FIBERS  = 3;
 const int MAX_VALUE = 10;
 
+// NOTE: This is for auto-destruction, argueably better as a std::unique_ptr
+//       custom deleter...
+struct FiberGroup
+{
+  ~FiberGroup()
+  {
+    for (auto &f : activeFibers)
+      f.join();
+  }
+
+  std::vector<boost::fibers::fiber> activeFibers;
+};
+
+thread_local static std::function<void(int)> currentFiberTask;
+thread_local static FiberGroup fibers;
+thread_local static bool newTask = false;
+thread_local static int numFinishedFibers = 0;
+
+void fiber_fcn(int whichFiber)
+{
+  if (!newTask)
+    boost::this_fiber::yield();
+
+  std::cout << "starting task on fiber [" << whichFiber << "]" << std::endl;
+
+  currentFiberTask(whichFiber);
+
+  std::cout << "finished task on fiber [" << whichFiber << "]" << std::endl;
+
+  newTask = false;
+  std::cout << "incrementing num finished from fiber [" << whichFiber << "] "
+            << numFinishedFibers++ << std::endl;;
+}
+
 template <typename TASK_T>
 inline void concurrent_for(int nFibers, TASK_T&& fcn)
 {
-  std::vector<boost::fibers::fiber> fibers;
+  fibers.activeFibers.resize(nFibers);
 
-  for (int i = 0; i < nFibers; ++i)
-    fibers.emplace_back(fcn);
+  int whichFiber = 0;
+  for (auto &fiber : fibers.activeFibers)
+    fiber = boost::fibers::fiber(fiber_fcn, whichFiber++);
 
-  for (auto &f : fibers)
-    f.join();
+  numFinishedFibers = 0;
+
+  currentFiberTask = fcn;
+  newTask = true;
+
+  boost::this_fiber::yield();
 }
 
 int main()
 {
   int value = N_FIBERS;
 
-  concurrent_for(N_FIBERS, [&]() {
+  concurrent_for(N_FIBERS, [&](int whichFiber) {
     while (value < MAX_VALUE) {
+#if 0
       value--;
+#else
+      value++;
+#endif
 
-      std::cout << "fiber[" << boost::this_fiber::get_id() << "] "
+      std::cout << "fiber[" << whichFiber << "] "
                 << "value now is " << value << std::endl;
 
-      concurrent_for(2, [&]() {
+#if 0
+      concurrent_for(2, [&](int whichSubFiber) {
         value++;
-        std::cout << "...subfiber[" << boost::this_fiber::get_id() << "] "
+        std::cout << "...subfiber[" << whichSubFiber << "] "
                   << "value now is " << value << std::endl;
       });
+#endif
 
       boost::this_fiber::yield();
     }
   });
+
+  while (numFinishedFibers < N_FIBERS) {
+    std::cout << "# finished fibers: " << numFinishedFibers << std::endl;
+    boost::this_fiber::yield();
+  }
 
   std::cout << "...done" << std::endl;
 
